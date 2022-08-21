74d8122f8e6aaf5ca72dc35d2626a4eb5952f79b
qsprajshekhar <rajshekhar419@gmail.com>
2022-08-21 12:00:49 +0530

    sixth.......

--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -3,7 +3,9 @@ public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("true"+1);
+		
+		System.out.println("f");
+		System.out.println("dhbf");
 
 	}
 
